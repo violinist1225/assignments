020b01bc5dff64595c68dbe59892b900eb61167f
Maya Purcell <violinist1225@gmail.com>
2020-10-19 17:44:58 -0700

    json-deli-assignment

---
+++ json-deli/app.js
@@ -0,0 +1,58 @@
+const appet = ['garden salad', 'air fried wings', 'hummus']
+const coldSand= ['better than subway/s veggie delite', 'turkey club', 'beef bologna']
+const hotSand = ['grilled cheese', 'vegan delite grilled cheese', 'turkey melt']
+const desserts = ['coder/s brownies', 'pudding', 'sweet potato pie slice']
+const drinks = ['bottled water', 'cane sugar soda', 'cranberry juice']
+
+
+
+{
+    "data": [
+    
+{
+    "name": "garden salad",
+    "ingredients": ["lettuce", "cherry tomatoes", "red onions", "cucumbers" ]  ,
+    "description": "chef salad  made from locally sourced ingredients"
+    }
+,
+{
+    "name": "air fried wings",
+    "ingredients":   ["organic chicken wings", "sea salt", "black pepper", "lemon pepper", "olive oil"] ,
+    "description": "8 wings lightly fried in olive oil in an air fryer "
+}
+
+,
+
+{
+    "name": "hummus",
+    "ingredients":  ["chickpeas", "water", "sea salt"]   ,
+    "description": "organic chickbeans blended to a smooth consistency"
+} 
+    ]
+}
+
+    // "name": "",
+    // "ingredients":     ,
+    // "description": 
+
+    // "name": "",
+    // "ingredients":     ,
+    // "description": 
+
+    // "name": "",
+    // "ingredients":     ,
+    // "description": 
+
+    // "name": "",
+    // "ingredients":     ,
+    // "description": 
+
+    // "name": "",
+    // "ingredients":     ,
+    // "description": 
+
+    
+
+    // "name": "",
+    // "ingredients":     ,
+    // "description": 
