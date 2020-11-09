6926864d71ed7403e5e1896f5a2bb46f86afdf43
Maya Purcell <violinist1225@gmail.com>
2020-11-09 14:54:58 -0800

    axios-to-do-mod2-updated

--- axios/app3.js
+++ axios/app3.js
@@ -1,7 +1,7 @@
 const form = document.theform
 const inputElement = document.theform.title
 const descriptionElem = document.theform.description
-const pictureUrl = document.theform.picUrl      
+const imgUrl = document.theform.imgUrl
 const price = document.theform.price
 
 
@@ -14,100 +14,117 @@ renderItems()
 
 function addTodo(e) {
     e.preventDefault();
-    //prevents page from refreshing 
+    // prevents page from refreshing
     console.log(inputElement.value)
     const todoObj = {
         title: inputElement.value,
         description: descriptionElem.value,
-        pictureUrl: pictureUrl.value,
-        price: price.value 
-            // variable stores information we want in the object - in this case 'title' 
+        imgUrl: imgUrl.value,
+        price: price.value
+        // variable stores information we want in the object - in this case 'title'
     }
     inputElement.value = ""
     descriptionElem.value = ""
-    pictureUrl.value = ""
+    imgUrl.value = ""
     price.value = ""
-        // clears out input after it's been captured in above line. Otherwise, if this step is not done, it will keep inputs repeating on the DOM.
-        /* close, but this isn't what is preventing the repeats on the DOM, this just clears the input field so you can type in a new todo item without needing to delete the last one */
+    // clears out input after it's been captured in above line. Otherwise, if this step is not done, it will keep inputs repeating on the DOM.
+    /* close, but this isn't what is preventing the repeats on the DOM, this just clears the input field so you can type in a new todo item without needing to delete the last one */
     axios.post(apiUrl, todoObj)
-        //Unsure of the entirety of what the above line does. But, axios.post(a,t) grabs the axios link and gets its value to post. Then, ? 
-        /* sends the new todo (which we made on lines 16-19) to the server using the post api call*/
-        .then(() => renderItems()) /* this is triggered if the api post was successful - basically: if the API was successful THEN call this annonymous arrow function, which in turn calls our renderItems function. it passes in an argument which is an object containing info about the posting from the server, but we are not using it in our function so I omitted it.   */
-        .catch(error => console.log(error))
-        /* this is triggered if the api post failes. it receives info about the reason for the failure, which we log to the console */
+    // Unsure of the entirety of what the above line does. But, axios.post(a,t) grabs the axios link and gets its value to post. Then, ?
+    /* sends the new todo (which we made on lines 16-19) to the server using the post api call*/.then(() => renderItems()) /* this is triggered if the api post was successful - basically: if the API was successful THEN call this annonymous arrow function, which in turn calls our renderItems function. it passes in an argument which is an object containing info about the posting from the server, but we are not using it in our function so I omitted it.   */.catch(error => console.log(error))
+    /* this is triggered if the api post failes. it receives info about the reason for the failure, which we log to the console */
 }
 
-function renderItems() {
-    //This function is grabbing each item in array of objects and creating a list to show on DOM with properties.
+function renderItems() { // This function is grabbing each item in array of objects and creating a list to show on DOM with properties.
     axios.get(apiUrl).then(response => {
-            clearList();
-            response.data.forEach(
-                (itemObj) => {
-                    createItem(itemObj)
-                })
+        clearList();
+        response.data.forEach((itemObj) => {
+            createItem(itemObj)
         })
-        .catch(error => console.log(error))
+    }).catch(error => console.log(error))
 }
 
 function clearList() {
     while (listElem.childElementCount > 0) {
-        //while the condition of the list having children is greater than 0, remove the firstchild from list. So, that the list doesn't repeat the list again. And can simply add on extra list item, that will be appended to end of list. 
+        // while the condition of the list having children is greater than 0, remove the firstchild from list. So, that the list doesn't repeat the list again. And can simply add on extra list item, that will be appended to end of list.
         /* perfect */
         listElem.firstChild.remove()
     }
 }
 
 function createItem(itemObj) {
-    console.log(itemObj)
-    const newLi = document.createElement('li') //creates list item element
-    const newP = document.createElement('p') //creates p element
-    const checkbox = document.createElement('input') //creates checkbox witih input datatype
-    newP.className = itemObj.completed ? "done" : "not-done" //creates class name in Javascript for p tag and assigns it to tertiary boolean. If item status is complted, then assign done or not done class on DOM. 
-    checkbox.type = "checkbox" //makes checkbox type = checkbox, so default textbox won't be assigned. 
-    checkbox.checked = itemObj.completed //if check box = checked, 
+    //console.log(itemObj)
+    const newLi = document.createElement('li') // creates list item element
+    const h3 = document.createElement('h3') // creates p element
+    const h3Span = document.createElement('span')
+    h3Span.textContent = itemObj.description
+    h3Span.style.color = "purple"
+    h3Span.style.fontSize = "25px"
+
+
+    const checkbox = document.createElement('input') // creates checkbox witih input datatype
+    h3.className = itemObj.completed ? "done" : "not-done" // creates class name in Javascript for p tag and assigns it to tertiary boolean. If item status is complted, then assign done or not done class on DOM.
+
+    checkbox.type = "checkbox" // makes checkbox type = checkbox, so default textbox won't be assigned.
+    checkbox.checked = itemObj.completed // if check box = checked,
     checkbox.addEventListener("change", (e) => {
-        axios.put(apiUrl + "/" + itemObj._id, { completed: !itemObj.completed })
-            //any of the axios methods return a PROMISE, so then must use .then and .catch methods after each promise.
-            //DOM reflects Server always
-            //.Put updates server with new content. Takes 2 arguments: 1st is the url we want to change (specifically), 2nd: What to change content to? In this case, completed key -part of object on server is set to false.  Then .then method called. Which calls renderItems function (that updates the DOM) 
-            .then((response) => {
+        axios.put(apiUrl + "/" + itemObj._id, {
+            completed: ! itemObj.completed
+        })
+        // any of the axios methods return a PROMISE, so then must use .then and .catch methods after each promise.
+        // DOM reflects Server always
+        // .Put updates server with new content. Takes 2 arguments: 1st is the url we want to change (specifically), 2nd: What to change content to? In this case, completed key -part of object on server is set to false.  Then .then method called. Which calls renderItems function (that updates the DOM).then((response) => {
+            .then(response => {
                 renderItems()
             })
-            .catch((error) => {
-                console.log(error)
-            })
-    })
+        .catch((error) => {
+            console.log(error)
+        })
+    }) 
+    
 
     const deleteButton = document.createElement("button")
-    newP.textContent = itemObj.title
+    h3.textContent = itemObj.title
+    h3.style.fontSize = "30px"
+    h3Span.style.alignItems = "center"
     deleteButton.textContent = "Delete"
     deleteButton.style.backgroundColor = "yellow"
+
     const editButton = document.createElement("button")
     editButton.textContent = "Edit"
     editButton.addEventListener("click", e => {
         const newInputField = document.createElement("input")
-        newP.style.display = "none" //hides p tag (newP created earlier). Since changing CSS Property, have to use Style Property in dot notation! 
-        //STOPPED HERE 
+        h3.style.display = "none"
+        // hides p tag (newP created earlier). Since changing CSS Property, have to use Style Property in dot notation!
+        // STOPPED HERE FOR EDIT BUTTON
     })
     deleteButton.addEventListener("click", (e) => {
         removeId(itemObj._id)
     })
-    newLi.append(checkbox, newP, deleteButton)
-    listElem.append(newLi);
-
     
+    newLi.append(checkbox, h3, h3Span, deleteButton) // all elements can be added on one line, in ORDER of appeara
+    if (itemObj.imgUrl) {
+        const picture = document.createElement("img")
+        picture.src = itemObj.imgUrl
+        newLi.append(picture)
+    }
+    if(itemObj.price){
+        const price = document.createElement("h2")  
+        const priceSpan = document.createElement('span')
+        priceSpan.textContent = itemObj.price
+        priceSpan.style.color = "orange"
+        newLi.append(priceSpan)
+    }
+    listElem.append(newLi)
 }
 
 
 
 function removeId(id) {
     axios.delete(`${apiUrl}/${id}`)
-        //any of the axios methods return a PROMISE, so then must use .then and .catch methods after each promise.
-        .then((response) => {
-            renderItems();
-        })
-        .catch(error => console.log(error))
+    // any of the axios methods return a PROMISE, so then must use .then and .catch methods after each promise..then
+    .then(reponse => {  
+    renderItems();
+    })
+    .catch(error => console.log(error))
 }
-
-
-
