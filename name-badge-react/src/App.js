f42fc430f405fa37a4e5971443da524e5ae7915c
Maya Purcell <violinist1225@gmail.com>
2020-12-17 12:53:02 -0800

    name-badge-react final

--- name-badge-react/src/App.js
+++ name-badge-react/src/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Badge from "./Badge"
 
-//sudo code: 1. Set state (needs each property in it) 2. Create a form with various inputs 3. Connect form to setstate; Name properties = setState variables 4. this.state.firstName = Value       5. Make a badge component (Resusability is point of components)
+// sudo code: 1. Set state (needs each property in it) 2. Create a form with various inputs 3. Connect form to setstate; Name properties = setState variables 4. this.state.firstName = Value       5. Make a badge component (Resusability is point of components)
 
 class App extends React.Component {
     constructor() {
@@ -14,141 +14,169 @@ class App extends React.Component {
             phoneNumber: "",
             favoriteFood: "",
             aboutSection: "",
-            badges: [],
+            badges: []
         }
     }
 
-handleChange =(e) => {
-    const{value, name} = e.target
-    this.setState({[name]: value})
-    const enabled = this.state.firstName.length >=2 && 
-                    this.state.lastName.length >=2 &&
-                    this.state.email.length >=2 &&
-                    this.state.placeOfBirth.length >=2 &&
-                    this.state.phoneNumber.length >=2 &&
-                    this.state.favoriteFood.length >=2 &&
-                    this.state.aboutSection.length >=2 
-                   
-if(enabled === true ){
-    this.setState((prevState) => {
-        return {...prevState, disabled: false}
+    handleChange = (e) => {
+        const {value, name} = e.target
+        this.setState({[name]: value})
+        const enabled = this.state.firstName.length >= 2 && this.state.lastName.length >= 2 && this.state.email.length >= 2 && this.state.placeOfBirth.length >= 2 && this.state.phoneNumber.length >= 2 && this.state.favoriteFood.length >= 2 && this.state.aboutSection.length >= 2
+
+        if (enabled === true) {
+            this.setState((prevState) => {
+                return {
+                    ...prevState,
+                    disabled: false
+                }
+            })
+        } else {
+            this.setState((prevState) => {
+                return {
+                    ...prevState,
+                    disabled: true
+                }
+            })
+        }
     }
-    )}
-    else{
-       this.setState((prevState) => {
-           return {...prevState, disabled: true}
-       })
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.setState((prevState) => {
+            return {
+                badges: [
+                    ...prevState.badges, {
+                        firstName: this.state.firstName,
+                        lastName: this.state.lastName,
+                        email: this.state.email,
+                        placeOfBirth: this.state.placeOfBirth,
+                        phoneNumber: this.state.phoneNumber,
+                        favoriteFood: this.state.favoriteFood,
+                        aboutSection: this.state.aboutSection,
+                        badges: this.state.badges,
+                        disabled: this.state.disabled
+                    }
+                ]
+            }
+
+        })
+
+        // handleSubmit method could house object for badge instead of component, if I want
+        // to access info again wouldn't be able to with current code. Ex:
+
+
     }
-}
-
-handleSubmit = (e) => {
-    e.preventDefault()
-    this.setState((prevState) =>{
-       return {badges: [...prevState.badges, {
-        firstName: this.state.firstName,
-        lastName: this.state.lastName,
-        email: this.state.email,
-        placeOfBirth: this.state.placeOfBirth,
-        phoneNumber: this.state.phoneNumber,
-        favoriteFood: this.state.favoriteFood,
-        aboutSection: this.state.aboutSection,
-        badges: this.state.badges,
-        disabled: this.state.disabled 
-       }]}
-    
-    })
-    
-    // handleSubmit method could house object for badge instead of component, if I want
-    // to access info again wouldn't be able to with current code. Ex: 
-
-        
-}
 
 
-render() {
-    return(
-        <div>
-            <div id="container">
-                <div id="innerbox">
-                    <form id="inputStyles" onSubmit={this.handleSubmit}>
-                        <input type="text"
-                        name="firstName"
-                        placeHolder="First Name"
-                        value={this.state.firstName}
-                        onChange={this.handleChange}
-                        />
+    render() {
+        return (
+            <div>
+                <div id="container">
+                    <div id="innerbox">
+                        <form id="inputStyles"
+                            onSubmit={
+                                this.handleSubmit
+                        }>
+                            <input type="text" name="firstName" placeHolder="First Name"
+                                value={
+                                    this.state.firstName
+                                }
+                                onChange={
+                                    this.handleChange
+                                }/>
 
-                        <input type="text"
-                        name="lastName"
-                        placeHolder="Last Name"
-                        value={this.state.lastName}
-                        onChange={this.handleChange}
-                        />
+                            <input type="text" name="lastName" placeHolder="Last Name"
+                                value={
+                                    this.state.lastName
+                                }
+                                onChange={
+                                    this.handleChange
+                                }/>
 
-                        <input type="email"
-                        name="email"
-                        value={this.state.email}
-                        placeHolder="Email"
-                        onChange={this.handleChange}
-                        />
+                            <input type="email" name="email"
+                                value={
+                                    this.state.email
+                                }
+                                placeHolder="Email"
+                                onChange={
+                                    this.handleChange
+                                }/>
 
-                        <input type="text"
-                        name="placeOfBirth"
-                        placeHolder="Place of Birth"
-                        value={this.state.placeOfBirth}
-                        onChange={this.handleChange}
-                        />
+                            <input type="text" name="placeOfBirth" placeHolder="Place of Birth"
+                                value={
+                                    this.state.placeOfBirth
+                                }
+                                onChange={
+                                    this.handleChange
+                                }/>
 
-                        <input type="text"
-                        name="phoneNumber"
-                        placeHolder="Phone Number"
-                        value={this.state.phoneNumber}
-                        onChange={this.handleChange}
-                        />
-                        
-                        <input type="text"
-                        name="favoriteFood"
-                        placeHolder="Favorite Food"
-                        value={this.state.favoriteFood}
-                        onChange={this.handleChange}
-                        />
+                            <input type="text" name="phoneNumber" placeHolder="Phone Number"
+                                value={
+                                    this.state.phoneNumber
+                                }
+                                onChange={
+                                    this.handleChange
+                                }/>
 
-                        
-                        <textarea id="textAreaWidth"
-                        name="aboutSection"
-                        placeHolder="About Section"
-                        value={this.state.aboutSection}
-                        onChange={this.handleChange} 
-                        rows="5">
-                        </textarea>
+                            <input type="text" name="favoriteFood" placeHolder="Favorite Food"
+                                value={
+                                    this.state.favoriteFood
+                                }
+                                onChange={
+                                    this.handleChange
+                                }/>
 
 
-                    <button id="submitButton"
-                     type="submit"
-                     disabled={this.state.disabled}
-                     >Submit </button>
-                    </form>
+                            <textarea id="textAreaWidth" name="aboutSection" placeHolder="About Section"
+                                value={
+                                    this.state.aboutSection
+                                }
+                                onChange={
+                                    this.handleChange
+                                }
+                                rows="5"></textarea>
+
+
+                            <button id="submitButton"
+                                //  type="submit"
+                                disabled={
+                                    this.state.disabled
+                            }>Submit
+                            </button>
+                        </form>
+                    </div>
                 </div>
-            </div>
-            {/* //this.state.badges is a security checkpoint that checks to see if it exists */}
-            {this.state.badges && this.state.badges.length >0 && this.state.badges.map(badge =>
-    {   <Badge 
-        firstName = {badge.firstName}
-        lastName  = {badge.lastName}
-        email     = {badge.email}
-        phoneNumber = {badge.phoneNumber}
-        placeOfBirth = {badge.placeOfBirth}
-        favoriteFood = {badge.favoriteFood}
-        aboutSection = {badge.aboutSection}
-        />
-    })
-}
+                {/* //this.state.badges is a security checkpoint that checks to see if it exists */}
+                {
+                this.state.badges && this.state.badges.length > 0 && this.state.badges.map(badge => {
+                   return ( <Badge firstName={
+                            badge.firstName
+                        }
+                        lastName={
+                            badge.lastName
+                        }
+                        email={
+                            badge.email
+                        }
+                        phoneNumber={
+                            badge.phoneNumber
+                        }
+                        placeOfBirth={
+                            badge.placeOfBirth
+                        }
+                        favoriteFood={
+                            badge.favoriteFood
+                        }
+                        aboutSection={
+                            badge.aboutSection
+                        }/>
+                )})
+                
+            }
 
 
-
-    {/* {this.state.charCountWarning ? <p>"You Need To Enter More than 3 Characters for each field!</p>: <> </>} */}
-        </div>
-    )
+                {/* {this.state.charCountWarning ? <p>"You Need To Enter More than 3 Characters for each field!</p>: <> </>} */} </div>
+        )
     }
 }
 
--- name-badge-react/src/Badge.js
+++ name-badge-react/src/Badge.js
@@ -6,17 +6,17 @@ function Badge(props){
             <div class="header">Badge</div>
             <div class="innerboxBadge">
                 <div id="left">
-                    <h3> First Name: {props.data.firstName} </h3>
-                    <h3> Last Name: {props.data.lastName} </h3>
-                    <h3> Email Address: {props.data.email} </h3>
+                    <h3> First Name: {props.firstName} </h3>
+                    <h3> Last Name: {props.lastName} </h3>
+                    <h3> Email Address: {props.email} </h3>
                 </div>
                 <div id="right">
-                    <h3> Place of Birth: {props.data.placeOfBirth} </h3>
-                    <h3> Phone Number: {props.data.phoneNumber} </h3>
-                    <h3> Favorite Food: {props.data.favoriteFood} </h3>
+                    <h3> Place of Birth: {props.placeOfBirth} </h3>
+                    <h3> Phone Number: {props.phoneNumber} </h3>
+                    <h3> Favorite Food: {props.favoriteFood} </h3>
                 </div>
             </div>
-            <p id="similarTextArea">{props.data.aboutSection}</p>
+            <p id="similarTextArea">{props.aboutSection}</p>
         </div>
     )    
 
